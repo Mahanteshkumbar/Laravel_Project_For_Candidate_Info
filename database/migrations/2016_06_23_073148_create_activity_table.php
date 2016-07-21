e6685f422ceb9bbe859d929ebfc799031665849f
Manu <mahantesh.kumbar9@gmail.com>
2016-07-21 17:58:35 +0530

    added auditingtrail library to the hobby model

---
+++ database/migrations/2016_06_23_073148_create_activity_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateActivityTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Activities', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('LoggedIn');
+            $table->integer('users_id')->unsigned();
+            $table->foreign('users_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('Activities');
+    }
+}
